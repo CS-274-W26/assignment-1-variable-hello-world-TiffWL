01d8ee2e677a0467f0f170b3234546d2743406ec
Tiffany <tiffanywua85@gmail.com>
2026-01-06 18:47:49 -0800

    Test

--- prog1.c
+++ prog1.c
@@ -1,6 +1,7 @@
 #include <stdio.h> // Necessary for printf()
-
 // TODO Other includes
+#include <cstdlib>
+#include <ctime>
 
 
 int main() {
@@ -8,4 +9,20 @@ int main() {
 	// is executed, it will automatically start here.
 	
 	// TODO Complete the program
+    srand(time(NULL));
+    int myrand = rand() % 100;
+
+    printf("The value is %i.\n", myrand);
+
+    if (0 <= myrand <= 4) {
+        printf("Eat more beef, kick less cats");
+    }
+    else if (5 <= myrand <= 9) {
+        printf("FRODO LIVES\n");
+    }
+    else {
+        printf("Larn is the best roguelike\n")
+    }
+    printf("The value is %i.\n", myrand);
+
 }
